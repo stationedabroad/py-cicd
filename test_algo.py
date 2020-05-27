90ee3f465d691b7af0299b61dbe5da6bf82b02a7
sulman shaikh <sulmanmh@gmail.com>
2020-05-27 23:07:43 +0100

    refactor out value  not found test

--- test_algo.py
+++ test_algo.py
@@ -4,6 +4,8 @@ def test_search():
     assert(binary_search(7, [0, 1, 2, 3, 4, 6, 7, 10]) == 6), "Found needle somewhere in haystack"
     assert(binary_search(1, [0, 1, 2, 3, 4, 6, 7, 10]) == 1), "Found needle somewhere in haystack"
     assert(binary_search(7, [0, 1, 2, 3, 4, 6, 7, 10]) == 6), "Found needle somewhere in haystack"
+
+def test_value_not_found():
     try:
         _ = binary_search(100, [0, 1, 2, 3, 4, 6, 7, 10])
     except ValueError as e:
