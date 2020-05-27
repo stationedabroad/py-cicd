80cb3504c8219b677aa90e05bdc42f59785183dd
sulman shaikh <sulmanmh@gmail.com>
2020-05-27 23:12:11 +0100

    refactor out first and last list value tests

--- test_algo.py
+++ test_algo.py
@@ -1,9 +1,10 @@
 from binary_search import binary_search
 
-def test_search():
-    assert(binary_search(7, [0, 1, 2, 3, 4, 6, 7, 10]) == 6), "Found needle somewhere in haystack"
-    assert(binary_search(1, [0, 1, 2, 3, 4, 6, 7, 10]) == 1), "Found needle somewhere in haystack"
-    assert(binary_search(7, [0, 1, 2, 3, 4, 6, 7, 10]) == 6), "Found needle somewhere in haystack"
+def test_search_first():
+    assert(binary_search(0, [0, 1, 2, 3, 4, 6, 7, 10]) == 0), "Found needle somewhere in haystack"
+
+def test_search_last():
+    assert(binary_search(10, [0, 1, 2, 3, 4, 6, 7, 10]) == 7), "Found needle somewhere in haystack"    
 
 def test_value_not_found():
     try:
