2bc2f01ba1311bfc78cd0c7fe6ff5f997cbd97b0
sulman shaikh <sulmanmh@gmail.com>
2020-05-29 00:04:16 +0100

    Test doco for b-search

--- test_algo.py
+++ test_algo.py
@@ -1,12 +1,15 @@
 from binary_search import binary_search
 
 def test_search_first():
+    "Test first element look up"
     assert(binary_search(0, [0, 1, 2, 3, 4, 6, 7, 10]) == 0), "Found needle somewhere in haystack"
 
 def test_search_last():
+    "Test last element look up"
     assert(binary_search(10, [0, 1, 2, 3, 4, 6, 7, 10]) == 7), "Found needle somewhere in haystack"    
 
 def test_value_not_found():
+    "Test value not found - return value error"
     try:
         _ = binary_search(100, [0, 1, 2, 3, 4, 6, 7, 10])
     except ValueError as e:
