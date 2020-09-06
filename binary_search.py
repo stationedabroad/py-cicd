d429040c7448bc9086d4d19dfeb1264da77d3bf0
sulman shaikh <sulmanmh@gmail.com>
2020-09-06 23:30:38 +0100

    binary search code clean up, removal of old funcs

--- binary_search.py
+++ binary_search.py
@@ -1,23 +1,5 @@
 A = [-1, 1,2,3,4,5,6,7,8,9,10, 101]
 
-# def search(needle, haystack):
-#     left = 0
-#     right = len(haystack)
-
-#     while True:
-#         middle = left + (right - left) // 2
-#         middle_value = haystack[middle]
-#         if middle_value == needle:
-#             return middle
-#         if right - left in [0, 1]:
-#             break
-#         elif middle_value < needle:
-#             left = middle
-#         else:
-#             right = middle
-#         print(f"L={left}\tR={right}\tM={middle}")            
-#     raise ValueError(f"Value {needle} not in haystack")
-
 def binary_search(needle, haystack):
     left = 0
     right = len(haystack) - 1
